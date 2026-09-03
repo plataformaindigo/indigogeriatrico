7a04f893f5d513d8a14cead8c43ae2b4f5888148
plataformaindigo <jorgemartingrassi@gmail.com>
2026-09-03 16:01:30 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
 
   API_URL:
-    'https://script.google.com/macros/s/AKfycbwNbFvYYOPnu2_KJYhi-3YmjxKw_VYYGjT-hfSN1ED9/dev',
+    'https://script.google.com/macros/s/AKfycbzqT9FOLBMk0wS-AwaSku2d7KEHEg3MTHlCqR8f6YwLetCFBeaQVnWn82plkjItSHEaNA/exec',
 
   SESSION_DURATION_MS:
     10 * 60 * 1000
